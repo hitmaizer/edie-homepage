a629f935a4b35f9973a9a25671bfd009d824f7c6
hitmaizer <jose.sousalves@gmail.com>
2022-02-04 15:17:15 +0000

    IntersectionObserver

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ hamburger.addEventListener('click', () => {
     hamburgerDiv.classList.toggle("open")
 })
 
+const sectionOne = document.getElementById('hero')
+const options = {}
+
+const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }
+        hamburgerDiv.classList.remove("open")
+        observer.unobserve(hamburgerDiv)
+    })
+}, options)
+
+observer.observe(sectionOne)
 
